f9ddfaed57523faa622ca78878c8d9a47f7bd333
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-18 11:05:41 -0300

    Export a create function

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
     {name: "Vitor", email: "vitor@gmail.com"},
@@ -7,5 +8,15 @@ const users = [
 export default {
     async index(req: Request, res: Response) {
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+
+        emailService.sendMail(
+            {name: 'Vitor', email: 'vitor@gmail.com'},
+            {subject: 'Bem-vindo ao sistema', body: 'Seja bem vindo'}
+        );
+        return res.json(emailService);
     }
 };
